2dc62a0730f16efce2382d82508356817cb93b1a
MrizalMuhaimin <13519136@std.stei.itb.ac.id>
2022-05-22 21:43:07 +0700

    add sidebar

---
+++ src/components/Menu/index.js
@@ -0,0 +1,10 @@
+function Menu(){
+    return(
+        <div className="flex justify-center items-center mb-2">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 stroke-gray-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h7" />
+            </svg> 
+        </div>
+    )
+}
+export default Menu;
--- src/components/MenuEmpty/index.js
+++ src/components/MenuEmpty/index.js
@@ -1,13 +1,13 @@
 import IconDashboar from '../../assets/icons/Dashboard_icon.svg'
 
-function MenuDashboard(){
+function MenuEmpty(){
     return(
         <div className=" w-20 h-12 hover:bg-gray-3 flex justify-center items-center group ">
             <div className=" group-hover:bg-white bg-gray-2 rounded w-7 h-7 flex justify-center items-center ">
-                <img src={IconDashboar} alt=""  className="rounded w-5 h-5  "/>
+                {/* <img src={IconDashboar} alt=""  className="rounded w-7 h-7 "/> */}
             </div>
         </div>
     )  
 }
 
-export default MenuDashboard;
+export default MenuEmpty;
